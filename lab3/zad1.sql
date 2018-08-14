5807d0e6baadc1181f9050b6ca7b455d4ed5601a
Tay0108 <kjkosecki@gmail.com>
2018-08-14 16:14:18 +0200

    lab3 zad1-2

--- lab3/zad1.sql
+++ lab3/zad1.sql
@@ -3,9 +3,26 @@
 -- Napisz zapytanie w języku SQL wyświetlające informacje na temat zamówień (idZamowienia, dataRealizacji), które mają być zrealizowane:
 
 -- 1. między 12 i 20 listopada 2013,
+    select idzamowienia, datarealizacji from zamowienia where datarealizacji >= '2013-11-12' and datarealizacji <= '2013-11-20';
+
 -- 2. między 1 i 6 grudnia lub między 15 i 20 grudnia 2013
+    select idzamowienia, datarealizacji from zamowienia where (datarealizacji >= '2013-12-01' and datarealizacji <= '2013-12-06') or (datarealizacji >= '2013-12-15' and datarealizacji <= '2013-12-20');
+
 -- 3. w grudniu 2013 (nie używaj funkcji date_part ani extract),
+    select idzamowienia, datarealizacji from zamowienia where (datarealizacji >= '2013-12-01' and datarealizacji <= '2013-12-31');
+
 -- 4. w listopadzie 2013 (w tym i kolejnych zapytaniach użyj funkcji date_part lub extract),
+    select idzamowienia, datarealizacji from zamowienia where date_part('month', datarealizacji) = '11' and date_part('year', datarealizacji) = '2013';
+    select idzamowienia, datarealizacji from zamowienia where extract(month from datarealizacji) = '11' and extract(year from datarealizacji) = '2013';
+
 -- 5. ★ w listopadzie lub grudniu 2013,
+    select idzamowienia, datarealizacji from zamowienia where date_part('month', datarealizacji) in ('11', '12') and date_part('year', datarealizacji) = '2013';
+    select idzamowienia, datarealizacji from zamowienia where extract(month from datarealizacji) in ('11', '12') and extract(year from datarealizacji) = '2013';
+
 -- 6. ★ 17, 18 lub 19 dnia miesiąca,
+    select idzamowienia, datarealizacji from zamowienia where date_part('day', datarealizacji) in ('17', '18', '19') and extract(year from datarealizacji) = '2013';
+    select idzamowienia, datarealizacji from zamowienia where extract(day from datarealizacji) in ('17', '18', '19') and extract(year from datarealizacji) = '2013';
+
 -- 7. ★ 46 lub 47 tygodniu roku.
+    select idzamowienia, datarealizacji from zamowienia where date_part('week', datarealizacji) in ('46', '47');
+    select idzamowienia, datarealizacji from zamowienia where extract(week from datarealizacji) in ('46', '47');
--- lab3/zad2.sql
+++ lab3/zad2.sql
@@ -3,12 +3,40 @@
 -- Napisz zapytanie w języku SQL wyświetlające informacje na temat czekoladek (idCzekoladki, nazwa, czekolada, orzechy, nadzienie), których nazwa:
 
 -- 1. rozpoczyna się na literę 'S',
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where nazwa like 'S%';
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where nazwa ~ '^S';
+
 -- 2. rozpoczyna się na literę 'S' i kończy się na literę 'i',
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where nazwa ~ '^S.*i$';
+
 -- 3. rozpoczyna się na literę 'S' i zawiera słowo rozpoczynające się na literę 'm',
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where nazwa ~ '^S.* m';
+
 -- 4. rozpoczyna się na literę 'A', 'B' lub 'C',
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where nazwa ~ '^(A|B|C)';
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where nazwa similar to 'A%|B%|C%';
+
 -- 5. zawiera rdzeń 'orzech' (może on wystąpić na początku i wówczas będzie pisany z wielkiej litery),
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki where nazwa ~ '(O|o)rzech';
+
 -- 6. ★ rozpoczyna się na literę 'S' i zawiera w środku literę 'm',
+    select idczekoladki, nazwa, czekolada, orzechy, nadzienie from czekoladki  where nazwa ~ '^S.*m';
+
 -- 7. ★ zawiera słowo 'maliny' lub 'truskawki',
+    select idczekoladki, nazwa, czekolada, orzechy nadzienie from czekoladki where nazwa ~ 'maliny|truskawki';
+    select idczekoladki, nazwa, czekolada, orzechy nadzienie from czekoladki where nazwa similar to '%maliny%|%truskawki%';
+
 -- 8. ★ nie rozpoczyna się żadną z liter: 'D'-'K', 'S' i 'T',
+    select idczekoladki, nazwa, czekolada, orzechy nadzienie from czekoladki where nazwa !~ '^(D-K|S|T)';
+
 -- 9. ★ rozpoczyna się od 'Slod' ('Słod'),
+    select idczekoladki, nazwa, czekolada, orzechy nadzienie from czekoladki where nazwa like 'Slod%';
+    select idczekoladki, nazwa, czekolada, orzechy nadzienie from czekoladki where nazwa ~ '^Slod';
+
 -- 10. ★ składa się dokładnie z jednego słowa.
+    select idczekoladki, nazwa, czekolada, orzechy nadzienie from czekoladki where nazwa !~ ' '; -- nie zawiera spacji
+
+-- TIP: ~ to inaczej like, tylko że dzialaja w nim wyrazenia regularne. Lepiej uzywac like jak sie da, bo jest szybszy.
+-- !~ to not like
+-- ~* to ilike (czyli case insensitive like)
+-- !~* to not ilike (czyli case insensitive like)
